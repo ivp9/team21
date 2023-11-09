73cbed91196239726835b0a3f2c5f673c8af5494
egorlitin <egorulitin@gmail.com>
2023-11-09 22:56:24 +0300

    add github action for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Это проект
+        </p>
+        <p
+          className="App-link"
+        >
+          Team 21
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
