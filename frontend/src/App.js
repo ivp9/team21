40d2e42460230f37297cadbc7611e2932b64088c
egorlitin <egorulitin@gmail.com>
2023-11-11 20:22:35 +0300

    add Card, CardsTable, utils, icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { icons } from './icons';
+import styled from 'styled-components';
+import { prepareCards } from './utils/prepareCards';
+import CardsTable from './CardsTable'
+
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh; 
+  background-color: gray;
+`;
+
+const CardContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(4, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+  gap: 8px;
+`;
 
 function App() {
+  const preparedIcons = prepareCards(icons);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Это проект
-        </p>
-        <p
-          className="App-link"
-        >
-          Team 21
-        </p>
-      </header>
-    </div>
+    <Container>
+      <CardContainer>
+        <CardsTable icons={preparedIcons} />
+      </CardContainer>
+    </Container>
   );
 }
 
